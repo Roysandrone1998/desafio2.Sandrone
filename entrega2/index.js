ef69076530893575c02398b8a5a0ce7405a93a83
Roysandrone1998 <raysandrone@gmail.com>
2023-11-13 19:27:33 -0300

    guardando

--- entrega2/index.js
+++ entrega2/index.js
@@ -5,13 +5,13 @@ class ProductManager {
     this.fileName = fileName;
     if (fs.existsSync(fileName)) {
       try {
-        let usuarios = fs.readFileSync(fileName, "utf-8");
-        this.usuarios = JSON.parse(usuarios);
+        let productos = fs.readFileSync(fileName, "utf-8");
+        this.productos = JSON.parse(productos);
       } catch (error) {
-        this.usuarios = [];
+        this.productos = [];
       }
     } else {
-      this.usuarios = [];
+      this.productos = [];
     }
   }
 
@@ -19,7 +19,7 @@ class ProductManager {
     try {
       await fs.promises.writeFile(
         this.fileName,
-        JSON.stringify(data, null, "\t")
+        JSON.stringify(data, null, 2)  
       );
       return true;
     } catch (error) {
@@ -28,26 +28,48 @@ class ProductManager {
     }
   }
 
-  async addProducto(usuario) {
-    this.usuarios.push(usuario);
+  async addProducto(producto) {
+    producto.id = this.generateId();
 
-    const respuesta = await this.saveFile(this.usuarios);
+    this.productos.push(producto);
+
+    const respuesta = await this.saveFile(this.productos);
 
     if (respuesta) {
-      console.log("todo bien");
+      console.log("Producto agregado correctamente");
     } else {
-      console.log("Hubo un error ");
+      console.log("Hubo un error al agregar el producto");
     }
   }
 
-  consultarUsuarios() {
-    console.log(this.usuarios);
-    return this.usuarios;
+  async deleteProducto(id) {
+    const index = this.productos.findIndex((producto) => producto.id === id);
+
+    if (index !== -1) {
+      this.productos.splice(index, 1);
+      await this.saveFile(this.productos);
+      console.log("Producto eliminado correctamente");
+    } else {
+      console.log("Producto no encontrado");
+    }
+  }
+
+  consultarProductos() {
+    console.log(this.productos);
+    return this.productos;
+  }
+
+  generateId() {
+    const maxId = this.productos.reduce(
+      (max, producto) => (producto.id > max ? producto.id : max),
+      0
+    );
+    return maxId + 1;
   }
 }
 
-class Usuario {
-  constructor(id, title, descripcion, code,stock) {
+class Producto {
+  constructor(id, title, descripcion, code, stock) {
     this.id = id;
     this.title = title;
     this.descripcion = descripcion;
@@ -57,16 +79,19 @@ class Usuario {
 }
 
 // Pruebas
-const producto1 = new Usuario(1, "Funda1","falsadescripcion1", 1, 30);
-const producto2 = new Usuario(2, "Funda2","falsadescripcion2", 2, 30);
-const producto3 = new Usuario(3, "Funda3","falsadescripcion3", 3, 30);
+const producto1 = new Producto(1, "Funda1", "falsadescripcion1", 1, 30);
+const producto2 = new Producto(2, "Funda2", "falsadescripcion2", 2, 30);
+const producto3 = new Producto(3, "Funda3", "falsadescripcion3", 3, 30);
 
-const manager = new ProductManager("./Usuarios.json");
+const manager = new ProductManager("./Productos.json");
 
-// console.log(manager.consultarUsuarios());
 manager.addProducto(producto1);
-console.log(manager.consultarUsuarios());
+console.log(manager.consultarProductos());
 
 manager.addProducto(producto2);
 manager.addProducto(producto3);
-console.log(manager.consultarUsuarios());
+console.log(manager.consultarProductos());
+
+// Eliminar el producto con id 2
+manager.deleteProducto(2);
+console.log(manager.consultarProductos());
