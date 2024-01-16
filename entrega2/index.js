3a0fdd72f7ca955283cee9014dfd16679c8bb010
Roysandrone1998 <raysandrone@gmail.com>
2024-01-16 20:56:20 -0300

    trabajandoo

--- entrega2/index.js
+++ entrega2/index.js
@@ -1,56 +1,80 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
 class ProductManager {
   constructor(fileName) {
     this.fileName = fileName;
-    if (fs.existsSync(fileName)) {
-      try {
-        let productos = fs.readFileSync(fileName, "utf-8");
+    this.productos = [];
+    this.loadProducts();
+  }
+
+  async loadProducts() {
+    try {
+      if (await fs.access(this.fileName)) {
+        const productos = await fs.readFile(this.fileName, "utf-8");
         this.productos = JSON.parse(productos);
-      } catch (error) {
-        this.productos = [];
       }
-    } else {
+    } catch (error) {
+      console.error("Error al cargar productos:", error);
       this.productos = [];
     }
   }
 
   async saveFile(data) {
     try {
-      await fs.promises.writeFile(
+      await fs.writeFile(
         this.fileName,
-        JSON.stringify(data, null, 2)  
+        JSON.stringify(data, null, 2)
       );
       return true;
     } catch (error) {
-      console.log(error);
+      console.error("Error al guardar archivo:", error);
       return false;
     }
   }
 
   async addProducto(producto) {
-    producto.id = this.generateId();
+    try {
+      if (!producto || typeof producto !== "object") {
+        throw new Error("El producto proporcionado no es válido");
+      }
 
-    this.productos.push(producto);
+      // Validar campos obligatorios
+      const camposObligatorios = ["title", "description", "price", "thumbnail", "code", "stock"];
+      camposObligatorios.forEach((campo) => {
+        if (!(campo in producto)) {
+          throw new Error(`El campo '${campo}' es obligatorio`);
+        }
+      });
 
-    const respuesta = await this.saveFile(this.productos);
+      producto.id = this.generateId();
 
-    if (respuesta) {
-      console.log("Producto agregado correctamente");
-    } else {
-      console.log("Hubo un error al agregar el producto");
+      this.productos.push(producto);
+
+      const respuesta = await this.saveFile(this.productos);
+
+      if (respuesta) {
+        console.log("Producto agregado correctamente");
+      } else {
+        console.log("Hubo un error al agregar el producto");
+      }
+    } catch (error) {
+      console.error("Error al agregar el producto:", error);
     }
   }
 
   async deleteProducto(id) {
-    const index = this.productos.findIndex((producto) => producto.id === id);
+    try {
+      const index = this.productos.findIndex((producto) => producto.id === id);
 
-    if (index !== -1) {
-      this.productos.splice(index, 1);
-      await this.saveFile(this.productos);
-      console.log("Producto eliminado correctamente");
-    } else {
-      console.log("Producto no encontrado");
+      if (index !== -1) {
+        this.productos.splice(index, 1);
+        await this.saveFile(this.productos);
+        console.log("Producto eliminado correctamente");
+      } else {
+        console.log("Producto no encontrado");
+      }
+    } catch (error) {
+      console.error("Error al eliminar el producto:", error);
     }
   }
 
@@ -69,19 +93,20 @@ class ProductManager {
 }
 
 class Producto {
-  constructor(id, title, descripcion, code, stock) {
-    this.id = id;
+  constructor(title, description, price, thumbnail, code, stock) {
     this.title = title;
-    this.descripcion = descripcion;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
     this.code = code;
     this.stock = stock;
   }
 }
 
 // Pruebas
-const producto1 = new Producto(1, "Funda1", "falsadescripcion1", 1, 30);
-const producto2 = new Producto(2, "Funda2", "falsadescripcion2", 2, 30);
-const producto3 = new Producto(3, "Funda3", "falsadescripcion3", 3, 30);
+const producto1 = new Producto("Funda1", "falsadescripcion1", 29.99, "imagen1.jpg", "P001", 30);
+const producto2 = new Producto("Funda2", "falsadescripcion2", 39.99, "imagen2.jpg", "P002", 20);
+const producto3 = new Producto("Funda3", "falsadescripcion3", 19.99, "imagen3.jpg", "P003", 25);
 
 const manager = new ProductManager("./Productos.json");
 
